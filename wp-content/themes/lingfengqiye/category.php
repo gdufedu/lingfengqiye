23e976220ca44f546e4231f0cd9f0b5458403f41
zhaoqiaoli <780325930@qq.com>
2019-04-16 23:00:40 +0800

    feat:add home page

---
+++ wp-content/themes/lingfengqiye/category.php
@@ -0,0 +1,10 @@
+<?php get_header();?>
+<div class="banner">
+    <img src="<?php echo get_template_directory_uri();?>/images/default-banner.jpg" alt="">
+</div><!-- .banner -->
+<div class="fenlei">
+    <div class="sidebar"></div><!-- .sidebar -->
+    <div class="contentr"></div><!-- .contentr -->
+    <div class="clear"></div><!-- .clear -->
+</div><!-- .fenlei -->
+<?php get_footer();?>
--- wp-content/themes/lingfengqiye/header.php
+++ wp-content/themes/lingfengqiye/header.php
@@ -1,80 +1,78 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?php wp_title('-', true, 'right'); ?></title>
+<title><?php wp_title('-',true,'right'); ?></title>
 <?php wp_head(); ?>
 </head>
-
 <body>
-<div class="wrap">
-	<div class="header">
-		<div class="header-top">
-			<h1 class="top-left">
-				<a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" /></a>
-			</h1><!-- .top-left -->
-			<div class="top-right">
-				<div class="top-nav">
-					<?php
-					/*
-					wp_nav_menu( $args )
-					@参数 array $args, 传递此参数时用array(成员参数名=>成员参数值)
-					特别说明：
-					调用导航菜单时，可以直接复制以下代码。然后根据需要删除成员参数
-					*/
-					wp_nav_menu( array(
-					  'theme_location'	=> 'daohangding',			//[保留]
-					  'menu'					=> '',									//[可删]
-					  'container'				=> false,							//[可删]
-					  'container_class'	=> '',									//[可删]
-					  'container_id'		=> '',									//[可删]
-					  'menu_class'		=> 'menu',						//[可删]
-					  'menu_id'				=> '',									//[可删]
-					  'echo'					=> true,							//[可删]
-					  'fallback_cb'			=> 'wp_page_menu',		//[可删]
-					  'before'					=> '',									//[可删]
-					  'after'						=> '',									//[可删]		
-					  'link_before'			=> '',									//[可删]
-					  'link_after'				=> '',									//[可删]
-					  'items_wrap'			=> '<ul id="%1$s" class="%2$s">%3$s</ul>',	//[可删]
-					  'depth'					=> 0,								//[可删]
-					  'walker'					=> ''									//[可删]			
-					) );
-					?>
-					
-				</div><!-- .top-nav -->
-				<div class="top-search">
-					<?php get_search_form(); ?>
-				</div><!-- .top-search -->
-			</div><!-- .top-right -->
-			<div class="clear"></div>
-		</div><!-- .header-top -->
-		<div class="header-nav">
-			<?php 
-			/*
-			wp_nav_menu( $args )
-			@参数 array $args, 传递此参数时用array(成员参数名=>成员参数值)
-			特别说明：
-			调用导航菜单时，可以直接复制以下代码。然后根据需要删除成员参数
-			*/
-			wp_nav_menu( array(
-			  'theme_location'	=> 'daohangzhu',			//[保留]
-			  'menu'					=> '',									//[可删]
-			  'container'				=> false,							//[可删]
-			  'container_class'	=> '',									//[可删]
-			  'container_id'		=> '',									//[可删]
-			  'menu_class'		=> 'menu',						//[可删]
-			  'menu_id'				=> '',									//[可删]
-			  'echo'					=> true,							//[可删]
-			  'fallback_cb'			=> 'wp_page_menu',		//[可删]
-			  'before'					=> '',									//[可删]
-			  'after'						=> '',									//[可删]		
-			  'link_before'			=> '',									//[可删]
-			  'link_after'				=> '',									//[可删]
-			  'items_wrap'			=> '<ul id="%1$s" class="%2$s">%3$s</ul>',	//[可删]
-			  'depth'					=> 0,								//[可删]
-			  'walker'					=> ''									//[可删]			
-			) );
-			?>		
-		</div><!-- .header-nav -->
-	</div><!-- .header -->
+  <div class="wrap">
+    <div class="header">
+      <div class="header-top">
+        <h1 class="top-left">
+          <a href="<?php bloginfo('url');?>"><img src="<?php echo get_template_directory_uri();?>/images/logo.png" alt=""></a>
+        </h1><!-- .top-left -->
+        <div class="top-right">
+          <div class="top-nav">
+            <?php
+            /*
+            wp_nav_menu( $args )
+            @参数 array $args, 传递此参数时用array(成员参数名=>成员参数值)
+            特别说明：
+            调用导航菜单时，可以直接复制以下代码。然后根据需要删除成员参数
+            */
+            wp_nav_menu( array(
+              'theme_location'  => 'daohangding',                  //[保留]
+              'menu'          => '',                  //[可删]
+              'container'       => false,             //[可删]
+              'container_class' => '',                  //[可删]
+              'container_id'    => '',                  //[可删]
+              'menu_class'    => 'menu',            //[可删]
+              'menu_id'       => '',                  //[可删]
+              'echo'          => true,              //[可删]
+              'fallback_cb'     => 'wp_page_menu',    //[可删]
+              'before'          => '',                  //[可删]
+              'after'           => '',                  //[可删]
+              'link_before'     => '',                  //[可删]
+              'link_after'        => '',                  //[可删]
+              'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',  //[可删]
+              'depth'         => 0,               //[可删]
+              'walker'          => ''                 //[可删]
+            ) );
+            ?>
+          </div><!-- .top-nav -->
+          <div class="top-search">
+            <?php get_search_form();?>
+          </div><!-- .top-search -->
+        </div><!-- .top-right -->
+        <div class="clear"></div>
+      </div><!-- .header-top -->
+      <div class="header-nav">
+        <?php
+        /*
+        wp_nav_menu( $args )
+        @参数 array $args, 传递此参数时用array(成员参数名=>成员参数值)
+        特别说明：
+        调用导航菜单时，可以直接复制以下代码。然后根据需要删除成员参数
+        */
+        wp_nav_menu( array(
+          'theme_location'  => 'daohangzhu',                  //[保留]
+          'menu'          => '',                  //[可删]
+          'container'       => false,             //[可删]
+          'container_class' => '',                  //[可删]
+          'container_id'    => '',                  //[可删]
+          'menu_class'    => 'menu',            //[可删]
+          'menu_id'       => '',                  //[可删]
+          'echo'          => true,              //[可删]
+          'fallback_cb'     => 'wp_page_menu',    //[可删]
+          'before'          => '',                  //[可删]
+          'after'           => '',                  //[可删]
+          'link_before'     => '',                  //[可删]
+          'link_after'        => '',                  //[可删]
+          'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',  //[可删]
+          'depth'         => 0,               //[可删]
+          'walker'          => ''                 //[可删]
+        ) );
+        ?>
+      </div><!-- .header-nav -->
+    </div><!-- .header -->
